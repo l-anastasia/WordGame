2049c7d26e8d146f4ee9aa9448dc48ad3ac53357
Anastasia Lobureva <amlobureva@gmail.com>
2021-08-21 21:01:38 +0300

    handlers work correctly dict needs checking

--- bot.py
+++ bot.py
@@ -21,13 +21,17 @@ def new_game(message):
     user_id = message.from_user.id
     all_words = prepare_all_words_list("words.txt")
     game_dict[user_id] = WordGame(all_words)
+    bot.send_message(message.from_user.id, user_id)
+    bot.send_message(message.from_user.id, game_dict)
     bot.send_message(message.from_user.id, 'enter your word, please')
 
 
 @bot.message_handler(commands=['surrender'])
 def surrender(message):
-    del game_dict[user_id]
-    bot.send_message(message.from_user.id, 'thank you for the game, if you want to play again type /new game')
+    # user_id = message.from_user.id
+    bot.send_message(message.from_user.id, game_dict)
+    del game_dict[message.from_user.id]
+    bot.send_message(message.from_user.id, 'thank you for the game, if you want to play again type /newgame')
 
 
 @bot.message_handler(content_types=['text'])
@@ -39,8 +43,9 @@ def play_game(message):
     # to create /new_game - create a new game for user_id
     # to create /surrender - finish the game
     word = message.text.lower()
-
-    verdict = game_dict[message.from_user_id].check_word(word)
+    # user_id = message.from_user.id
+    bot.send_message(message.from_user.id, message.from_user.id)
+    verdict = game_dict[message.from_user.id].check_word(word)
     if verdict is WordGame.Verdict.NOT_WORD:
         bot.send_message(message.from_user.id, 'This is not a word')
         return
@@ -53,20 +58,20 @@ def play_game(message):
         bot.send_message(message.from_user.id, 'Your word is not correct, try again')
         return
 
-    answer = game_dict[message.from_user_id].next_word(word)
+    answer = game_dict[message.from_user.id].next_word(word)
 
     if answer == 2:
         bot.send_message(message.from_user.id, 'You won')
-        del game_dict[message.from_user_id]
-        bot.send_message(message.from_user.id, 'thank you for the game, if you want to play again type /new game')
+        del game_dict[message.from_user.id]
+        bot.send_message(message.from_user.id, 'thank you for the game, if you want to play again type /newgame')
 
-    if answer:
+    if answer != 2:
         bot.send_message(message.from_user.id, answer)
-        game_dict[message.from_user_id].prev_word = answer
+        game_dict[message.from_user.id].prev_word = answer
 
 
 # if __name__ == "__main__":
-#     main(message)
+#     main()
 
 
 bot.polling(none_stop=True, interval=0)
